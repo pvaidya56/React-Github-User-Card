70bcdf9eb6cb502603357a791ce67241cbcf84a8
Priya <pvaidya56@gmail.com>
2020-01-17 19:24:02 -0800

    Fetch data and display

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import Card from './components/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>This is from app.js</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      pvaidya56: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/pvaidya56')
+      .then(res => {
+        // console.log(res);
+        this.setState({ pvaidya56: res.data})
+        // console.log(this.state)
+      })
+    .catch(err => console.log("Data was not returned", err));
+  }
+  
+  render(){
+    return (
+     <>
+     <Card user={this.state["pvaidya56"]}/>
+     </>
+    );
+  }
+  
 }
 
 export default App;
